3c64bf687b7f6d068eeda818ea53242fef96a95c
Sanjay kumar <sanjaykumar2763@gmail.com>
2018-09-02 15:31:24 +0530

    Checkout UI

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,3 +1,12 @@
-$( "#other" ).click(function(event) {
-	$( "#target" ).click();
+$(".add-to-cart").click(function(event) {
+	const temp = Math.random( );
+	const alertHTML = `
+		<div class="cart-alert alert alert-success alert-dismissible mb-5">
+			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+			Item added to cart
+		</div>
+	`;
+	const alertElement = $(alertHTML);
+	alertElement.delay(1000).fadeOut(700, function () { $(this).remove(); });
+	alertElement.appendTo("#alerts").fadeIn('slow');
 });
